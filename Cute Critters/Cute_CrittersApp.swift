4e9581eac499f3059149237d1db9f453dd7e4ad4
Reilly <odonnell.reilly.j@gmail.com>
2024-12-25 11:39:00 -0500

    Initial Commit

---
+++ Cute Critters/Cute_CrittersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cute_CrittersApp.swift
+//  Cute Critters
+//
+//  Created by Reilly O’Donnell on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Cute_CrittersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cute CrittersTests/Cute_CrittersTests.swift
@@ -0,0 +1,16 @@
+//
+//  Cute_CrittersTests.swift
+//  Cute CrittersTests
+//
+//  Created by Reilly O’Donnell on 12/25/24.
+//
+
+import Testing
+
+struct Cute_CrittersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
